26da7268fdcfba19848fad4f3ac7672827392337
Vasudha <Vasudhagarwal10618@gmail.com>
2026-06-02 17:18:28 +0530

    Time: 0 ms (100.00%) | Memory: 91.7 MB (99.16%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -4,6 +4,7 @@ public:
         int n_land = landStartTime.size();
         int n_water = waterStartTime.size();
 
+        //First - land ride then water ride
         int water_2 = INT_MAX;
         int land_1 = INT_MAX;
         for(int i=0 ; i<n_land ; i++)
@@ -15,6 +16,7 @@ public:
             water_2 = min(water_2, max(waterStartTime[i] , land_1)+waterDuration[i]);
         }
 
+        //Second - water ride then land ride
         int land_2 = INT_MAX;
         int water_1 = INT_MAX;
         for(int i=0 ; i<n_water ; i++)
