5b6826cf5e9b1e6ace0bbaa1a76495e4bcbb7182
Vasudha <Vasudhagarwal10618@gmail.com>
2026-06-02 17:17:11 +0530

    Time: 0 ms (100.00%) | Memory: 91.9 MB (59.92%) - LeetSync

--- 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
+++ 3965-earliest-finish-time-for-land-and-water-rides-i/earliest-finish-time-for-land-and-water-rides-i.cpp
@@ -1,30 +1,31 @@
 class Solution {
 public:
     int earliestFinishTime(vector<int>& landStartTime, vector<int>& landDuration, vector<int>& waterStartTime, vector<int>& waterDuration) {
-        int mini=INT_MAX;
-        int n=landStartTime.size();
-        int m=waterStartTime.size();
+        int n_land = landStartTime.size();
+        int n_water = waterStartTime.size();
 
-        for(int i=0;i<n;i++){
-            int ls=landStartTime[i];
-            int ld=landDuration[i];
-            for(int j=0;j<m;j++){
-                int ws=waterStartTime[j];
-                int wd=waterDuration[j];
-
-                int s1=max(ls,0);
-                int e1=s1+ld;
-                int s2=max(ws,e1);
-                int total1=s2+wd;
-                mini=min(mini,total1);
-
-                int s3=max(ws,0);
-                int e3=s3+wd;
-                int s4=max(ls,e3);
-                int total2=s4+ld;
-                mini=min(mini,total2);
-            }
+        int water_2 = INT_MAX;
+        int land_1 = INT_MAX;
+        for(int i=0 ; i<n_land ; i++)
+        {
+            land_1 = min(land_1 , landStartTime[i] + landDuration[i]);
         }
-        return mini;
+        for(int i=0 ; i<n_water ; ++i)
+        {
+            water_2 = min(water_2, max(waterStartTime[i] , land_1)+waterDuration[i]);
+        }
+
+        int land_2 = INT_MAX;
+        int water_1 = INT_MAX;
+        for(int i=0 ; i<n_water ; i++)
+        {
+            water_1 = min(water_1 , waterStartTime[i] + waterDuration[i]);
+        }
+        for(int i=0 ; i<n_land ; i++)
+        {
+            land_2 = min(land_2 , max(landStartTime[i] , water_1) + landDuration[i]);
+        }
+
+        return min(land_2 , water_2);
     }
 };
