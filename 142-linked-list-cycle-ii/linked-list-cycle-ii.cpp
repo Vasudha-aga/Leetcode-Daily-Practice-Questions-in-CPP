b72f710353b7fb20a2ae36eb1a43fc4f28519aa4
Vasudha <Vasudhagarwal10618@gmail.com>
2026-08-29 15:49:34 +0530

    Time: 5 ms (82.89%) | Memory: 11.4 MB (23.54%) - LeetSync

--- 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/linked-list-cycle-ii.cpp
@@ -8,15 +8,15 @@
  */
 class Solution {
 public:
-    ListNode *detectCycle(ListNode *head) {
+    ListNode *detectCycle(ListNode *head) { //O(n) , O(1)
         ListNode* fast = head;
         ListNode* slow = head;
         while(fast != NULL && fast->next != NULL){
             fast = fast->next->next;
             slow = slow->next;
 
-            if(fast == slow){
-                slow=head;
+            if(fast == slow){ //there is a cycle in the list
+                slow=head; //reset the slow pointer to head of the list and move both pointers
                 while(slow!=fast){
                     slow = slow->next;
                     fast = fast->next;
