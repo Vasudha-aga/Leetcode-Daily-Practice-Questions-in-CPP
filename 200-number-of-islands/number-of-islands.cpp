4b4ecfa8aa4b81fc827d49e9b99925fdfb6a1459
Vasudha <Vasudhagarwal10618@gmail.com>
2026-08-31 21:52:59 +0530

    Time: 26 ms (62.95%) | Memory: 16.3 MB (80.75%) - LeetSync

--- 200-number-of-islands/number-of-islands.cpp
+++ 200-number-of-islands/number-of-islands.cpp
@@ -1,5 +1,6 @@
 class Solution {
 public:
+//whenever we encounter '1' we have discovered an island then DFS visits the entire conected '1' (island) and convert it to '0' so that we don't count it again
     int numIslands(vector<vector<char>>& grid) {
         int m = grid.size() , n = m?grid[0].size():0, islands =0;
         for(int i =0 ; i<m ; i++){
@@ -13,7 +14,7 @@ public:
         return islands;
     }
 private:
-    void eraseIslands(vector<vector<char>> &grid , int i , int j){
+    void eraseIslands(vector<vector<char>> &grid , int i , int j){ //find every piece of land connected to a cell and change it to water
         int m = grid.size() , n = grid[0].size();
         if(i<0 || i==m || j<0 || j==n || grid[i][j] == '0') return;
         grid[i][j] = '0';
