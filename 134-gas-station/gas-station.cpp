3bf937f4ea7a45d504f4c772fc135cc9d46a9e0c
Vasudha <Vasudhagarwal10618@gmail.com>
2026-09-02 13:59:42 +0530

    Time: 0 ms (100.00%) | Memory: 112.5 MB (61.48%) - LeetSync

--- 134-gas-station/gas-station.cpp
+++ 134-gas-station/gas-station.cpp
@@ -4,13 +4,16 @@ public:
         int totalgas = 0, totalcost =0;
         for(int i =0 ; i<gas.size() ; i++){
             totalgas += gas[i];
-            totalcost += cost[i];
+            totalcost += cost[i]; //total gas required
         }
-        if(totalgas < totalcost) return -1;
-        int currgas = 0;
-        int start =0;
+        //if totalgas = totalcost --> it is possible to traverse the circular route
+        if(totalgas < totalcost) return -1; //no matter from where you start the total amount of gas you collect around the entire circle is less than the total gas required --> no starting point
+
+        //if totalgas >= totalcost then the problem gaurantees a solution ie some valid starting station exists
+        int currgas = 0; //amount of gas we currently have
+        int start =0; //lets try start as station 1 ie idx 0
         for(int i =0 ; i<gas.size() ; i++){
-            currgas += gas[i] - cost[i];
+            currgas += gas[i] - cost[i]; //gas gained - gas spent should be positive to start the travel
             if(currgas < 0){
                 currgas = 0;
                 start = i+1;
