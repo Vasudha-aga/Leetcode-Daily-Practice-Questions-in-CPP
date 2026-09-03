f422903da4f6016ed30c4a62a55fdd683bb10067
Vasudha <Vasudhagarwal10618@gmail.com>
2026-09-03 11:17:08 +0530

    Time: 0 ms (100.00%) | Memory: 11.6 MB (79.25%) - LeetSync

--- 75-sort-colors/sort-colors.cpp
+++ 75-sort-colors/sort-colors.cpp
@@ -1,20 +1,20 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        //Dutch National Flag (DNF) algorithm
-        int high = nums.size() - 1;
-        int low = 0;
-        int mid = 0;
-        while (mid <= high) {
-            if (nums[mid] == 0) {
-                swap(nums[low], nums[mid]);
+        //Dutch National Flag Algorithm (DNF)
+        int high = nums.size() - 1; //keeps the track on 2
+        int low = 0; //keeps the track on 0
+        int mid  = 0; //keeps the track on 1
+        while(mid<=high){
+            if(nums[mid] == 0){
+                swap(nums[low] , nums[mid]);
                 low++;
                 mid++;
-            } else if (nums[mid] == 1) {
-                mid++;
-            } else {
-                swap(nums[high], nums[mid]);
+            }else if(nums[mid] == 2){
+                swap(nums[high] , nums[mid]);
                 high--;
+            }else{
+                mid++;
             }
         }
     }
